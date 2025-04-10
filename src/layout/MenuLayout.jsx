6a01dcfd11019714aacbaeeb0c998b02b3ad5553
taswakil <designerfairytasneem@gmail.com>
2025-04-10 14:53:57 -0500

    Update MenuLayout.jsx

--- src/layout/MenuLayout.jsx
+++ src/layout/MenuLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Menu from '../pages/Menu'
+
 
 const MenuLayout = () => {
   return (
     <div>
         <h2> Menu Items</h2>
         <p> List of current menu items at our cafe!^-^</p>
+      
         <Outlet/>
     </div>
   )
