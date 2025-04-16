c2130a0f420a46f141b14bfb56f69c69947ae427
taswakil <designerfairytasneem@gmail.com>
2025-04-15 19:30:07 -0500

    replaced code with updated

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,32 +1,27 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
-
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const MenuItems = () => {
-  
-   const MenuItems = useLoaderData();
+  const MenuItems = useLoaderData();
 
-    return (
+  return (
     <div>
-        <br/>
-        <h2>Dessert Category:</h2>
-        <h1>{MenuItems.dessert}</h1>
-        <p>
-            Indulge in delicious fluffy desserts that range in sweetness ^-^
-        </p>
-        <img src= {MenuItems.image}/>
+      <br />
+      <h2>Dessert Category:</h2>
+      <h1>{MenuItems.dessert}</h1>
+      <p>Indulge in delicious fluffy desserts that range in sweetness ^-^</p>
+      <img src={MenuItems.image} />
     </div>
-  )
-}
+  );
+};
+export default MenuItems;
 
-export default MenuItems
-
-export const MenuItemsLoader = async({params}) => {
-    const {id} = params;
-    // displaying the fetched URL from data.json and adding the ID endpoint 
-    const res = await fetch("http://localhost:3000/menu/" + id)
-    if(!res.ok){
-        throw Error('Could not find dessert details:(')
-    }
-    return res.json();
-}
+export const MenuItemsLoader = async ({ params }) => {
+  const { id } = params;
+  // displaying the fetched URL from data.json and adding the ID endpoint
+  const res = await fetch("http://localhost:3000/menu/" + id);
+  if (!res.ok) {
+    throw Error("Could not find dessert details:(");
+  }
+  return res.json();
+};
