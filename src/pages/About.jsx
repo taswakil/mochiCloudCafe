3e404c660f3defd044b6b317445f94f5bb182c2c
taswakil <designerfairytasneem@gmail.com>
2025-04-28 11:29:57 -0500

    putting content in the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
+    <>
     <div>
-        <h1>About Page</h1>
-        <p>Hello and welcome to about our cafe! ^-^</p>
+    <div style={{width: "100%"}}>
+        <img src="src/assets/abouthero.svg"/>
     </div>
+    <div className='box-box'>
+       <div className='container1'>
+        <h2>Who We Are</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
+
+            <br/>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
+       </div>
+
+       <div className='container2'>
+         
+       </div>
+    </div>
+    </div>
+    </>
   )
 }
 
