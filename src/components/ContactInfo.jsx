bc6901d6e6571e104b065574da365aaff1aec402
taswakil <designerfairytasneem@gmail.com>
2025-04-28 11:24:59 -0500

    adding the contact info page with synthetic info

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const ContactInfo = () => {
   return (
     <div>
-        <p>Email: example@gmail.com </p>
-        <p> Phone: (123)111-1111</p>
-        <p> Address: Rainbow Road 1234, Hidden Village</p>
-    </div>
-  )
-}
+      <div className="info">
+        <div className="info-items">
+          <h2>Email</h2>
+          <p className="textBg">contact@mochicloudcafe.com </p>
+        </div>
 
-export default ContactInfo
+        <div className="info-items">
+          <h2>Phone</h2>
+          <p className="textBg">(123)111-1111</p>
+        </div>
+
+        <div className="info-items">
+          <h2>Location</h2>
+          <p className="textBg">Rainbow Road 1234, Hidden Village</p>
+        </div>
+      </div>
+      <div className="notinfo">
+        <img src="src/assets/socails.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
